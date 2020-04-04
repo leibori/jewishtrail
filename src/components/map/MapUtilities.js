cead5647c3a1f9cf99f37e0f756db290c1e1c645
Amit Ashkenazy <amit471@gmail.com>
2020-04-05 00:01:43 +0300

    Added map utilities and map directory.

---
+++ src/components/map/MapUtilities.js
@@ -0,0 +1,8 @@
+import L from 'leaflet';
+
+export var myIcon = L.icon({
+    iconUrl: 'http://www.clker.com/cliparts/k/a/2/B/c/u/map-marker-red-th.png',
+    iconSize: [24.8, 39.6],
+    iconAnchor: [12.4, 39.6],
+    popupAnchor: [0, -30]
+});
