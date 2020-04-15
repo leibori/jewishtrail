95aa9938ea79a233b66199bafe58d6e05e7dc5f9
Amit Ashkenazy <amit471@gmail.com>
2020-04-15 17:22:32 +0300

    Remade the site and road components using "Grid" from "material-ui".

--- src/components/road/roadComponent.js
+++ src/components/road/roadComponent.js
@@ -1,24 +1,90 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
-const RoadComponent = ({props}) => {
-  var info_url = '/road/'+props.id
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: '100%',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  img: {
+    outline: 'none',
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+}));
+
+const buttonStyles = makeStyles({
+  button: {
+    textTransform: "none"
+  }
+})
+
+const RoadComponent = (props) => {
+  
+  const road = props.props.road
+  const buttonName = props.props.buttonName
+  const condition = props.props.condition
+  const buttonFunction = props.props.buttonFunction
+
+  var info_url = '/road/'+road.id
+  const classes = useStyles()
+  const buttonClasses = buttonStyles()
+
   return (
-    <div className="card z-depth-0 project-summary">
-    <div className="card-content grey-text text-darken-3">
-      <div className="site-name ">Road name: {props.name}</div>
-      <div className="site-city ">Cities: {props.city.join(", ")}.</div>
-      <div className="site-country ">Countries: {props.country.join(", ")}.</div>
-      {
-        <div className="container">
-            {props.tags.map((tag, i) => {
-               return (<span key = {i}> {tag} </span>) 
-            })}
-        </div>  
-      }
-      <Link to={info_url}>More info</Link>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        {/* <Link to={info_url} className="black-text"> */}
+        <Grid container spacing={2} direction='row'>
+          
+          <Grid item xs={6} lg>
+            <ButtonBase className={classes.image}>
+              <img className={classes.img} alt="complex" src={road.imageUrl} />
+            </ButtonBase>
+          </Grid>
+
+          <Grid item xs={6} sm container>
+
+            <Grid item xs container direction="column" spacing={2}>
+              
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1"><b>Trail name:</b> {road.name}</Typography>
+                <Typography variant="body2" gutterBottom><b>Cities:</b> {road.city.join(", ")}.</Typography>
+                <Typography variant="body2" ><b>Countries:</b> {road.country.join(", ")}.</Typography>
+                <Grid item container direction='row' spacing={2}>
+                  <Grid item xs={6}>
+                    <Button className={buttonClasses.button} variant="outlined" style={{color: '#b5d7c7', borderColor: '#b5d7c7', textAlign: 'center', height: '50px', marginTop: '10px'}} size="small" href={info_url}>View Trail</Button>
+                  </Grid>
+                  {condition ?
+                    <Grid item xs={6}>
+                      <Button className={buttonClasses.button} variant="outlined" style={{color: '#b5d7c7', borderColor: '#b5d7c7', textAlign: 'center', height: '50px', marginTop: '10px'}} size="small" onClick={(e) => buttonFunction(e, road.id)}>{buttonName}</Button>
+                    </Grid> : ''
+                  }
+                </Grid>
+              </Grid>
+
+            </Grid>
+
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
-  </div>
+
   )
 }
 
--- src/components/sites/siteComponent.js
+++ src/components/sites/siteComponent.js
@@ -1,24 +1,88 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
-const SiteComponent = ({props}) => {
-  var info_url = '/site/'+props.id
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: '100%',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  img: {
+    outline: 'none',
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+}));
+
+const buttonStyles = makeStyles({
+  button: {
+    textTransform: "none"
+  }
+})
+
+const SiteComponent = (props) => {
+  const site = props.props.site
+  const buttonName = props.props.buttonName
+  const condition = props.props.condition
+  const buttonFunction = props.props.buttonFunction
+
+  const info_url = '/site/'+site.id
+  const classes = useStyles()
+  const buttonClasses = buttonStyles()
+
   return (
-    <div className="card z-depth-0 project-summary">
-    <div className="card-content grey-text text-darken-3">
-      <div className="site-name ">Site name: {props.name}</div>
-      <div className="site-city ">City: {props.city}</div>
-      <div className="site-country ">Country: {props.country}</div>
-      {
-        <div className="container">
-            {props.tags.map((tag, i) => {
-               return (<span key = {i}> {tag} </span>) 
-            })}
-        </div>  
-      }
-      <Link to={info_url}>More info</Link>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        {/* <Link to={info_url} className="black-text"> */}
+        <Grid container spacing={2} direction='row'>
+          
+          <Grid item xs={6} lg>
+            <ButtonBase className={classes.image}>
+              <img className={classes.img} alt="complex" src={site.imageUrl} />
+            </ButtonBase>
+          </Grid>
+
+          <Grid item xs={6} sm container>
+
+            <Grid item xs container direction="column" spacing={2}>
+              
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1"><b>Site name:</b> {site.name}</Typography>
+                <Typography variant="body2" gutterBottom><b>City:</b> {site.city}.</Typography>
+                <Typography variant="body2" ><b>Country:</b> {site.country}.</Typography>
+                <Grid item container direction='row' spacing={2}>
+                  <Grid item xs={6}>
+                    <Button className={buttonClasses.button} variant="outlined" style={{color: '#b5d7c7', borderColor: '#b5d7c7', textAlign: 'center', height: '50px', marginTop: '10px'}} size="small" href={info_url}>View Site</Button>
+                  </Grid>
+                  {condition ?
+                    (<Grid item xs={6}>
+                      <Button className={buttonClasses.button} variant="outlined" style={{color: '#b5d7c7', borderColor: '#b5d7c7', textAlign: 'center', height: '50px', marginTop: '10px'}} size="small" onClick={(e) => buttonFunction(e, site.id)}>{buttonName}</Button>
+                    </Grid>) : ''
+                  }
+                </Grid>
+              </Grid>
+
+            </Grid>
+
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
-  </div>
   )
 }
 
