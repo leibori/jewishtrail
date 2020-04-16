bbb5fcaf3039a8512135f07f6f01b88af7e28e44
Ofek Atar <ofek.atr@gmail.com>
2020-04-16 20:13:01 +0300

    Set all routing paths to be lower-case

--- src/App.js
+++ src/App.js
@@ -72,31 +72,31 @@ class App extends Component {
               <Redirect to='/LoginPage'/>
             </Route>
             {/* <Route exact path='/' component={LoginPage}/> */}
-            <Route path='/Menu'  component={Menu} />
-            <Route path='/LoginPage' component={LoginPage} />
-            <Route path='/LogOut' component={LogOut} />
-            <Route path='/SignUp' component={SignUp} />
-            <Route path='/adminPage' component={AdminMenu} />
-            <Route path='/createSite' component={CreateSite} />
-            <Route exact path='/deleteSite' component={DeleteSite} />
-            <Route exact path='/deleteSite/:searchVal' component={DeleteSite} />
-            <Route exact path='/updateSite' component={UpdateSite} />
-            <Route exact path='/updateSite/:searchVal' component={UpdateSite} />
-            <Route exact path='/updateForm/:id' component={UpdateForm} />
+            <Route path='/menu'  component={Menu} />
+            <Route path='/loginpage' component={LoginPage} />
+            <Route path='/logout' component={LogOut} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/adminpage' component={AdminMenu} />
+            <Route path='/createsite' component={CreateSite} />
+            <Route exact path='/deletesite' component={DeleteSite} />
+            <Route exact path='/deletesite/:searchVal' component={DeleteSite} />
+            <Route exact path='/updatesite' component={UpdateSite} />
+            <Route exact path='/updatesite/:searchVal' component={UpdateSite} />
+            <Route exact path='/updateform/:id' component={UpdateForm} />
             <Route path='/auth' component={AuthMenu} />
             <Route exact path='/search' component={SearchMenu} />
             <Route exact path='/search/:searchVal' component={SearchMenu} />
             <Route path='/favorites' component={Favorites} />
             <Route path='/about' component={About} />
             <Route exact path='/site/:id' component={SitePage} />
-            <Route exact path='/RoadForm' component={RoadForm} />
-            <Route exact path='/RoadForm/:searchVal' component={RoadForm} />
+            <Route exact path='/roadform' component={RoadForm} />
+            <Route exact path='/foadform/:searchVal' component={RoadForm} />
             <Route exact path='/admin' component={AdminPage} />
-            <Route exact path='/searchRoad' component={RoadMenu} />
-            <Route exact path='/searchRoad/:searchVal' component={RoadMenu} />
+            <Route exact path='/searchroad' component={RoadMenu} />
+            <Route exact path='/searchroad/:searchVal' component={RoadMenu} />
             <Route exact path='/road/:id' component={RaodPage} />
-            <Route exact path='/deleteRoad' component={DeleteRoad} />
-            <Route exact path='/deleteRoad/:searchVal' component={DeleteRoad} />
+            <Route exact path='/deleteroad' component={DeleteRoad} />
+            <Route exact path='/deleteroad/:searchVal' component={DeleteRoad} />
             
             
           </Switch>
