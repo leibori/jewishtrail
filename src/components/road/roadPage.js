f6aaad61e52383b1bf53eba2be01374a92716120
Amit Ashkenazy <amit471@gmail.com>
2020-04-20 18:00:40 +0300

    Separated the "Component will mount to multiple methods, and added link to google maps with navigation of road.

--- src/components/road/roadPage.js
+++ src/components/road/roadPage.js
@@ -25,20 +25,42 @@ class RaodPage extends Component {
             siteList:[],
             haveUsersLocation: false,
             imageUrl: '',
-            zoom: 6
+            location: {
+                lat: 41.294856,
+                lng: -4.055685,
+            },
+            navigationLink: "https://www.google.com/maps/dir/?api=1"
         }
     };
 
-    async componentWillMount() {
-        const roadId = this.state.site_id;
-        var all_road_props = await getRoadByID(roadId)
-        console.log(all_road_props)
-        const siteListID = all_road_props.siteList;
-        const siteList = await Promise.all(siteListID.map((async (sid) => ({ id:sid, ...(await getSiteByID(sid))}))))
-        this.setState({
-                        ...all_road_props,
-                        siteList
+    async handleUserLocation() {
+        navigator.geolocation.getCurrentPosition((position) => {
+            fetch('https://ipapi.co/json')
+            .then(res => res.json())
+            this.setState({
+                location: {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude},
+                haveUsersLocation: true,
+            })
+        }, () => {
+            console.log('access denied')
+            fetch('https://ipapi.co/json')
+            .then(res => res.json())
+            .then(location => {
+                console.log(location)
+                this.setState({
+                    location: {
+                        lat: location.latitude,
+                        lng: location.longitude },
+                    haveUsersLocation: true,
+                })
+            })
         })
+    }
+
+    async handleMap() {
+        const { siteList } = this.state;
 
         const maxLat = Math.max.apply(Math, siteList.map(function(site) { return site.latitude }))
         const minLat = Math.min.apply(Math, siteList.map(function(site) { return site.latitude }))
@@ -49,11 +71,59 @@ class RaodPage extends Component {
         const avgLat = (maxLat + minLat) / 2
         const avgLng = (maxLng + minLng) / 2
 
-        getAroundYouMap('map', avgLat, avgLng, this.state.zoom, siteList)
+        const zoom = Math.floor(Math.max(maxLat - minLat, maxLng - minLng))        
+
+        getAroundYouMap('map', avgLat, avgLng, zoom, siteList)
+    }
+
+    async handleNavigationLink() {
+        const { siteList } = this.state;
+        var navLink = this.state.navigationLink + "&waypoints="
+        const firstSite = siteList[0], lastSite = siteList[siteList.length - 1]
+        var firstDistance = Math.sqrt(Math.pow(this.state.location.lat - firstSite.latitude, 2) + Math.pow(this.state.location.lng - firstSite.longitude, 2))
+        var lastDistance = Math.sqrt(Math.pow(this.state.location.lat - lastSite.latitude, 2) + Math.pow(this.state.location.lng - lastSite.longitude, 2))
+
+        if(firstDistance < lastDistance) {
+            navLink += firstSite.latitude + "%2C" + firstSite.longitude
+            for(var i = 1; i < siteList.length - 2; ++i) {
+                navLink += "%7C" + siteList[i].latitude + "%2C" + siteList[i].longitude
+            }
+            navLink += "&destination=" + lastSite.latitude + "%2C" + lastSite.longitude
+        } else {
+            navLink += lastSite.latitude + "%2C" + lastSite.longitude
+            for(var i = siteList.length - 2; i > 0; --i) {
+                navLink += "%7C" + siteList[i].latitude + "%2C" + siteList[i].longitude
+            }
+            navLink += "&destination=" + firstSite.latitude + "%2C" + firstSite.longitude
+        }
+        navLink += "&dir_action=navigate"
+        // console.log(navLink)
+        this.setState({
+            navigationLink: navLink
+        })
+    }
+
+    async componentWillMount() {
+
+        this.handleUserLocation()
+
+        const roadId = this.state.site_id;
+        var all_road_props = await getRoadByID(roadId)
+        console.log(all_road_props)
+        const siteListID = all_road_props.siteList;
+        const siteList = await Promise.all(siteListID.map((async (sid) => ({ id:sid, ...(await getSiteByID(sid))}))))
+        this.setState({
+            ...all_road_props,
+            siteList
+        })
+
+        this.handleMap()
+
+        this.handleNavigationLink()
     }
 
     render() {
-        const { siteList } = this.state;
+        const { siteList, navigationLink } = this.state;
         if(!siteList.length){
             return <span>Loading...</span>
         }
@@ -77,9 +147,8 @@ class RaodPage extends Component {
                     <Card.Img variant="top" src={imageUrl} />
                     <Card.Body>
                         <Card.Title>{this.state.name}</Card.Title>
-                        <Card.Text>
-                        {this.state.description}
-                        </Card.Text>
+                        <Card.Text>{this.state.description}</Card.Text>
+                        <Card.Link  onClick={() => window.open(navigationLink)} style={{ color: "#1295b1" }}>Start the journey</Card.Link>
                     </Card.Body>
                     <ListGroup className="list-group-flush">
                         <ListGroupItem className="container">
