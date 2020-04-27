9af8a9f2d5408c0c7a8e16185d28b53d845b2b89
Amit Ashkenazy <amit471@gmail.com>
2020-04-27 23:16:27 +0300

    Initializing the redux store in src/index.js.

--- src/index.js
+++ src/index.js
@@ -6,17 +6,22 @@ import registerServiceWorker from './registerServiceWorker';
 import * as serviceWorker from './serviceWorker';
 import 'leaflet/dist/leaflet.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './configureStore'
 
-ReactDOM.render(<App style={{zScore: '700'}}/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={ store }>
+        <PersistGate loading={null} persistor={persistor}>
+            <App style={{zScore: '700'}}/>
+        </PersistGate>
+    </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
-
-// Ask Ori about it 
-
