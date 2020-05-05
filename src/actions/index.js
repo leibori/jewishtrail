6f0c123d78ff6aed8759175e09d5f2e99c1a1163
Amit Ashkenazy <amit471@gmail.com>
2020-05-05 17:21:51 +0300

    Added action to redux for setting site and trail favorites IDs.

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,3 +8,17 @@ export const setLogStatus = (status) => {
         status
     }
 }
+
+export const setSiteFavorites = (favorites) => {
+    return {
+        type: 'SET_SITE_FAVORITES',
+        siteFavorites: favorites
+    }
+}
+
+export const setTrailFavorites = (favorites) => {
+    return {
+        type: 'SET_TRAIL_FAVORITES',
+        trailFavorites: favorites
+    }
+}
