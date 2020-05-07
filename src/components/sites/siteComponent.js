73d3b7876f1f2c9c7fbf686ee3c923087294bc14
Ofek Atar <ofek.atr@gmail.com>
2020-05-07 16:26:57 +0300

    Fix

--- src/components/sites/siteComponent.js
+++ src/components/sites/siteComponent.js
@@ -4,7 +4,6 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
-import './index.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
