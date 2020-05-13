1905fb29f8ca1cb6d4764e06145f17c89eec97cb
Ofek Atar <ofekatar98@gmail.com>
2020-05-14 00:13:45 +0300

    Added resend auth email option

--- src/components/login/NotVerified/index.js
+++ src/components/login/NotVerified/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './NotVerified.css';
-//
+import {emailAuthentication} from 'components/firebase/FirebaseLoginUtils';
+
 export default function NotVerified(props) {
     const text = "Please check your email to verify your account."
+    const sub_text = "resend email"
     return (
         <div style={{position: 'fixed', minWidth: '100%', minHeight: '100%', margin: 'auto', display: 'flex', justifyContent: 'center',
           alignItems: 'center'}}>
             <div class="container1">
-                <p class="info">{text}</p>
+                <header class="info">{text}<br/><a onClick={()=>emailAuthentication()} style={{textDecoration: 'underline', color: '#0000EE'}}>{sub_text}</a></header>
             </div>
         </div>
     );
