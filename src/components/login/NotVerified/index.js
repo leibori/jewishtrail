65a4158436a7fe0f7bcad571ca8f024a51a65aab
Amit Ashkenazy <amit471@gmail.com>
2020-05-29 18:35:34 +0300

    Added background to "not verified" page.

--- src/components/login/NotVerified/index.js
+++ src/components/login/NotVerified/index.js
@@ -7,9 +7,9 @@ export default function NotVerified(props) {
     const text = "Please check your email to verify your account."
     const sub_text = "resend email"
     return (
-        <div style={{position: 'fixed', minWidth: '100%', minHeight: '100%', margin: 'auto', display: 'flex', justifyContent: 'center',
+        <div className='bg-img' style={{position: 'fixed', minWidth: '100%', minHeight: '100%', margin: 'auto', display: 'flex', justifyContent: 'center',
           alignItems: 'center'}}>
-            <div class="container1">
+            <div className="container1">
                 <header class="info">{text}<br/><a onClick={()=>emailAuthentication()} style={{textDecoration: 'underline', color: '#00CED1'}}>{sub_text}</a></header>
             </div>
         </div>
